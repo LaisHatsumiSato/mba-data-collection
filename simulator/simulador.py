2a51cead5d87f35dc0761f9936a0c08fd9ddf58c
LaisHatsumiSato <56139707+LaisHatsumiSato@users.noreply.github.com>
2026-04-13 00:58:58 +0000

    correção dos códigos

--- simulator/simulador.py
+++ simulator/simulador.py
@@ -16,21 +16,33 @@ produtos = [
 
 metodos_pagamento = ["pix", "cartao_credito", "boleto"]
 
+# 🔥 memória de transações antigas (pra duplicar)
+historico = []
+
 while True:
     produto = random.choice(produtos)
 
-    transacao = {
-        "transacao_id": str(uuid.uuid4()),
-        "cliente_id": random.randint(1, 100),
-        "produto": produto["nome"],
-        "categoria": produto["categoria"],
-        "valor": round(random.uniform(50, 5000), 2),
-        "quantidade": random.randint(1, 3),
-        "data_transacao": datetime.now().isoformat(),
-        "metodo_pagamento": random.choice(metodos_pagamento)
-    }
+    # 🔁 20% de chance de duplicar
+    if historico and random.random() < 0.2:
+        transacao = random.choice(historico)
+        print("♻️ DUPLICADO GERADO:", transacao)
 
-    #  SIMULA ERRO DE DADOS (10% dos casos)
+    else:
+        transacao = {
+            "transacao_id": str(uuid.uuid4()),
+            "cliente_id": random.randint(1, 100),
+            "produto": produto["nome"],
+            "categoria": produto["categoria"],
+            "valor": round(random.uniform(50, 5000), 2),
+            "quantidade": random.randint(1, 3),
+            "data_transacao": datetime.now().isoformat(),
+            "metodo_pagamento": random.choice(metodos_pagamento)
+        }
+
+        # salva no histórico
+        historico.append(transacao)
+
+    # ❗ erro proposital
     if random.random() < 0.1:
         transacao["valor"] = None
 
